a2a7b72af65fccfcdc73487da2aadf2b42cd6683
Michael Chacaton <personal@chaksoft.fr>
2022-03-14 07:47:09 +0100

    fix outdated deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,49 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ['airbnb'],
-  rules: {
-    'no-console': 'error',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'comma-dangle': ['error', 'always-multiline'],
-    semi: ['error', 'never'],
-    'key-spacing': [
+  root : true,
+  env : { node : true },
+  extends : ['airbnb'],
+  rules : {
+    'no-console' : 'error',
+    'no-debugger' : process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle' : ['error', 'always-multiline'],
+    semi : ['error', 'never'],
+    'key-spacing' : [
       'error',
       {
-        beforeColon: true,
-        afterColon: true,
+        beforeColon : true,
+        afterColon : true,
       },
     ],
-    'space-before-function-paren': ['error', 'always'],
-    'arrow-parens': ['error', 'always'],
-    'eol-last': ['error', 'always'],
-    'object-property-newline': [
+    'space-before-function-paren' : ['error', 'always'],
+    'arrow-parens' : ['error', 'always'],
+    'eol-last' : ['error', 'always'],
+    'object-property-newline' : [
+      'error',
+      { allowAllPropertiesOnSameLine : false },
+    ],
+    'object-curly-newline' : [
       'error',
       {
-        allowAllPropertiesOnSameLine: false,
+        ObjectExpression : {
+          multiline : true,
+          minProperties : 2,
+        },
+        ObjectPattern : {
+          multiline : true,
+          minProperties : 2,
+        },
+        ImportDeclaration : {
+          multiline : true,
+          minProperties : 2,
+        },
+        ExportDeclaration : 'always',
       },
     ],
-    'object-curly-newline': [
-      'error',
-      {
-        ObjectExpression: { multiline: true, minProperties: 2 },
-        ObjectPattern: { multiline: true, minProperties: 2 },
-        ImportDeclaration: { multiline: true, minProperties: 2 },
-        ExportDeclaration: 'always',
-      },
-    ],
-    'max-len': ['error', 160],
-    'no-underscore-dangle': ['error', {
-      allow : ['_id'],
-    }],
-    'no-restricted-syntax': 'off',
-    'no-param-reassign': 'off',
-    'no-await-in-loop': 'off',
+    'max-len' : ['error', 160],
+    'no-underscore-dangle' : ['error', { allow : ['_id'] }],
+    'no-restricted-syntax' : 'off',
+    'no-param-reassign' : 'off',
+    'no-await-in-loop' : 'off',
     'import/no-dynamic-require' : 'off',
     'global-require' : 'off',
     'no-restricted-globals' : 'off',
@@ -50,11 +53,8 @@ module.exports = {
     'no-prototype-builtins' : 'off',
     'no-plusplus' : 'off',
     'no-case-declarations' : 'off',
-    'arrow-body-style': 'off',
-    'no-bitwise': 'off',
-    'no-underscore-dangle': 'off',
+    'arrow-body-style' : 'off',
+    'no-bitwise' : 'off',
   },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+  parserOptions : { parser : 'babel-eslint' },
 }
